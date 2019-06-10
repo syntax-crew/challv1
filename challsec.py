7feefea0510c305bb1badb0494615b142db524cb
syntax-crew <syntaxerrorcrew.id@gmail.com>
2019-06-11 01:22:32 +0700

    Create challsec.py

---
+++ challsec.py
@@ -0,0 +1,64 @@
+#! usr/bin/python
+
+#Challange Syntax Error Crew
+
+import os
+
+import sys
+
+os.system ('clear')
+
+os.system ('figlet ch4llSEC')
+
+sec = """- CHALL EASY LEVEL {} BUKTIKAN KAMU BISA!!!"""
+
+syn = """- SELAMAT MENCOBA CHALLnya {} SAYA TUNGGU DI TEAM KAMI -"""
+
+aks = """- Mulai => 687474703a2f2f77696e646d696c6c73616e64706f72637570696e65732e636f2e7a612f6368616c6c7365632e68746d6c"""
+
+print sec
+
+os.system ('sleep 3')
+
+rname = raw_input ("NAMA PANJANG  : ")
+
+rpang = raw_input ("Panggilan     : ")
+
+rrrrr = raw_input ("Umur          : ")
+
+fake1 = raw_input ("Alasan Gabung : ")
+
+os.system ('clear')
+
+os.system ('sleep 3')
+
+print "."
+
+print ".."
+
+print "..."
+
+print "...."
+
+print "....."
+
+print "......"
+
+print aks
+
+print syn
+
+print "......."
+
+print "......"
+
+print "....."
+
+print "...."
+
+print "..."
+
+print ".."
+
+print "."
+
